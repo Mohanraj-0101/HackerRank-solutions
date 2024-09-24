2042adaa4eb9fa29abebc7c234e6aa718145675e
Mohanraj-0101 <mohanraj.r.cse.2022@snsce.ac.in>
2024-09-24 20:52:46 +0530

    Add if-else program

---
+++ If-Else.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+     Scanner scanner = new Scanner(System.in);
+    int N = scanner.nextInt();
+    scanner.close();
+    
+    if(N%2==1 || (N%2==0 && N>=6 && N<=20)){
+        System.out.println("Weird");
+    }
+   else{
+       System.out.println("Not Weird");
+   }
+    }
+}
